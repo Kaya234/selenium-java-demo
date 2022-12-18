d4e5499cab224122e19c401949b98f22b5ffecc1
Kaya234 <112717648+Kaya234@users.noreply.github.com>
2022-12-18 11:21:33 +0100

    add BasePage

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,99 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.List;
+import static org.junit.Assert.assertTrue;
+
+
+public class BasePage {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+
+    public void visit(String url) {
+        driver.navigate().to(url);
+
+    }
+
+    public void type(By elementBy, String text){
+        wait.until(ExpectedConditions.presenceOfElementLocated(elementBy)).sendKeys(text);
+
+    }
+
+    public void clickOn(By elementBy){
+        wait.until(ExpectedConditions.presenceOfElementLocated(elementBy)).click();
+
+    }
+
+    public void clickContainText(String text){
+        wait.until(ExpectedConditions
+                        .presenceOfElementLocated(By.xpath("//span[text()= '" + text + "']")))
+                .click();
+
+    }
+
+    public void clickContainTextDropdown(String option){
+        wait.until(ExpectedConditions
+                        .presenceOfElementLocated(By.xpath("//div[span[text()= '" + option + "']]")))
+                .click();
+
+    }
+
+    public void assertTrueText( By elementBy, String text){
+        String actualString = wait.until(ExpectedConditions.presenceOfElementLocated(elementBy))
+                .getText();
+        assertTrue(actualString.contains(text));
+
+    }
+
+    public void assertTrueEmpty(By elementBy){
+        String actualString = wait.until(ExpectedConditions.presenceOfElementLocated(elementBy)).getText();
+        assertTrue(actualString.isEmpty());
+
+    }
+
+     public void assertTrueToast(String text){
+         String actualString = wait.until(ExpectedConditions
+                         .presenceOfElementLocated(By.xpath("//p[text()= '" + text + "']")))
+                 .getText();
+        assertTrue(actualString.contains(text));
+
+    }
+
+     public void assertTrueSearchResult(String text){
+         String actualString = wait.until(ExpectedConditions
+                         .presenceOfElementLocated(By.xpath("//div[span[text()= '" + text + "']]")))
+                 .getText();
+         assertTrue(actualString.contains(text));
+
+    }
+
+     public void assertTrueList(By elementBy, String role){
+         List<WebElement> rows = driver.findElements(elementBy);
+         for (WebElement row : rows) {
+             {
+                 assertTrue(row.getText().contains(role));
+             }
+
+         }
+
+    }
+
+     public void autocomplete(By elementBy) throws InterruptedException {
+         Thread.sleep(2000);
+         driver.findElement(elementBy).click();
+
+    }
+}
